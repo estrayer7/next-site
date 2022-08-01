014d0bc25a2e0a4bfcbc700433b63cadf9786ad7
Elijah Strayer <estrayer7@gmail.com>
2022-08-01 09:09:34 -0400

    fix forecast

--- pages/fishing.js
+++ pages/fishing.js
@@ -4,15 +4,15 @@ import Link from 'next/link'
 import axios from 'axios'
 import { BASE_URL } from '../utils/constants'
 
-const fishing = ({fish, temp}) => {
+const fishing = ({fish, forecast}) => {
   return (
     <>
     <div className='flex justify-center'>
-    <div className='border-black border-solid border-2 rounded w-[70vw]'>
+    <div className='border-black border-solid border-2 rounded w-[80vw]'>
     <h1 className='text-2xl text-center m-2'>Five Hour Forecast</h1>
       <div className='flex justify-center'>
-        {temp.properties.periods.slice(0,5).map((weather) => (
-          <div key={weather.number} className='mb-5 mr-5'>
+        {forecast.properties.periods.slice(0,5).map((weather) => (
+          <div key={weather.number} className='m-3 p-3 w-1/5'>
             <p>Temperature: {weather.temperature} F</p>
             <p>Cover: {weather.shortForecast}</p>
             <p>Wind: {weather.windDirection} at {weather.windSpeed}</p>
@@ -26,7 +26,7 @@ const fishing = ({fish, temp}) => {
         <div key={fishy._id}>
           <div className='relative w-[400px] h-[500px] grow basis-1/4 cursor-pointer'>
             <Link href={`/details/${fishy._id}`}>
-              <div>
+              <div className='relative w-[400px] h-[500px]'>
                 <Image src={fishy.imageUrl} alt='pic' layout='fill' objectFit='cover' className='rounded-md'/>
               </div>
             </Link>
@@ -47,6 +47,6 @@ export const getServerSideProps = async (context) => {
 	
   const res = await axios.get(`${BASE_URL}/api/fish`);
   return {
-    props: {fish: res.data, temp: weatherData}
+    props: {fish: res.data, forecast: weatherData}
   }
 }
